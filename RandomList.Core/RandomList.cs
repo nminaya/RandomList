70bcb90d46d2be1389aea83208e6c4aa5d34a123
Nelson Minaya <nelson_minaya@claro.com.do>
2018-03-21 13:24:55 -0400

    First commit
    Co-Authored-By: Nelson Minaya  <nelsonminayaperez@gmail.com>

---
+++ RandomList.Core/RandomList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace RandomList.Core
+{
+	public class RandomList<T> : IEnumerable<T> where T : class
+	{
+		public IEnumerator<T> GetEnumerator()
+		{
+			throw new NotImplementedException();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
