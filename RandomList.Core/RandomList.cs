25417526f48158f1926e7aced4fe5ff6c66e5867
Nelson Minaya <nelson_minaya@claro.com.do>
2018-03-21 14:07:32 -0400

    Improving RandomList
    Co-Authored-By: Nelson Minaya  <nelsonminayaperez@gmail.com>

--- RandomList.Core/RandomList.cs
+++ RandomList.Core/RandomList.cs
@@ -1,46 +1,71 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace RandomList.Core
 {
 	public class RandomList<T> : IList<T> where T : class
 	{
-		private readonly List<T> list = new List<T>();
+		private readonly List<T> _list = new List<T>();
+
+		private int _currentIndex = 0;
+
+		private int[] _randomIndexs;
 
 		public T this[int index]
 		{
-			get => list[index];
-			set => list[index] = value;
+			get => _list[_randomIndexs[index]];
+			set => _list[_randomIndexs[index]] = value;
 		}
 
-		public int Count => list.Count;
+		public int Count => _list.Count;
 
 		public bool IsReadOnly => false;
 
-		public void Add(T item) => list.Add(item);
+		public void Add(T item) => _list.Add(item);
 
-		public void Clear() => list.Clear();
+		public void Clear() => _list.Clear();
 
-		public bool Contains(T item) => list.Contains(item);
+		public bool Contains(T item) => _list.Contains(item);
 
-		public void CopyTo(T[] array, int arrayIndex) => list.CopyTo(array, arrayIndex);'
+		public void CopyTo(T[] array, int arrayIndex) => _list.CopyTo(array, arrayIndex);
 
-		public int IndexOf(T item) => list.IndexOf(item);
+		public int IndexOf(T item) => _list.IndexOf(item);
 
-		public void Insert(int index, T item) => list.Insert(index, item);
+		public void Insert(int index, T item) => _list.Insert(index, item);
 
-		public bool Remove(T item) => list.Remove(item);
+		public bool Remove(T item) => _list.Remove(item);
 
-		public void RemoveAt(int index) => list.RemoveAt(index);
+		public void RemoveAt(int index) => _list.RemoveAt(index);
 
 		IEnumerator IEnumerable.GetEnumerator()
 		{
-			throw new NotImplementedException();
+			return this.GetEnumerator();
 		}
+
 		public IEnumerator<T> GetEnumerator()
 		{
-			throw new NotImplementedException();
+			for (int i = 0; i < _list.Count; i++)
+			{
+				yield return _list[_randomIndexs[i]];
+			}
+		}
+
+		private IEnumerable<int> GetRandom()
+		{
+			var nums = Enumerable.Range(0, _list.Count).ToArray();
+			var rnd = new Random();
+
+			for (int i = 0; i < nums.Length; ++i)
+			{
+				int randomIndex = rnd.Next(nums.Length);
+				int temp = nums[randomIndex];
+				nums[randomIndex] = nums[i];
+				nums[i] = temp;
+			}
+
+			return nums;
 		}
 	}
 }
