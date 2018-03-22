1f846e53ccf0584913da4fd34185614e99ff3e75
Nelson Minaya <nelson_minaya@claro.com.do>
2018-03-22 13:59:10 -0400

    Adding some unit tests
    Co-Authored-By: Nelson Minaya <nelsonminayaperez@gmail.com>

--- RandomList.Tests/RandomListTest.cs
+++ RandomList.Tests/RandomListTest.cs
@@ -1,4 +1,5 @@
-using System;
+using System.Collections.Generic;
+using System.Linq;
 using RandomList.Core;
 using Xunit;
 
@@ -27,6 +28,116 @@ namespace RandomList.Tests
 
 			// Assert
 			Assert.True(1 == randList.Count);
+		}
+
+		[Fact]
+		public void Adding_100Items()
+		{
+			// Arrange
+			var randList = new RandomList<int>();
+
+			// Act
+			for (int i = 0; i < 100; i++)
+			{
+				randList.Add(i);
+			}
+
+			// Assert
+			Assert.True(100 == randList.Count);
+		}
+
+		[Fact]
+		public void Adding_1000Items()
+		{
+			// Arrange
+			var randList = new RandomList<int>();
+
+			// Act
+			for (int i = 0; i < 1000; i++)
+			{
+				randList.Add(i);
+			}
+
+			// Assert
+			Assert.True(1000 == randList.Count);
+		}
+
+		[Fact]
+		public void Removing_OneItem()
+		{
+			// Arrange
+			var randList = new RandomList<int> { 1 };
+
+			// Act
+			randList.Remove(1);
+
+			// Assert
+			Assert.True(0 == randList.Count);
+		}
+
+		[Fact]
+		public void Removing_10Items()
+		{
+			// Arrange
+			var randList = new RandomList<int>();
+
+			for (int i = 0; i < 10; i++)
+			{
+				randList.Add(i);
+			}
+
+			// Act
+			for (int i = 0; i < 10; i++)
+			{
+				randList.Remove(i);
+			}
+
+			// Assert
+			Assert.True(0 == randList.Count);
+		}
+
+		[Fact]
+		public void ClearList()
+		{
+			// Arrange
+			var randList = new RandomList<int>();
+
+			for (int i = 0; i < 10; i++)
+			{
+				randList.Add(i);
+			}
+
+			// Act
+			randList.Clear();
+
+			// Assert
+			Assert.True(0 == randList.Count);
+		}
+
+		[Fact]
+		public void Contains_TrulyContains()
+		{
+			// Arrange
+			var randList = new RandomList<int>() { 5 };
+
+			// Act
+			bool result = randList.Contains(5);
+
+			// Assert
+			Assert.True(result);
+		}
+
+		[Fact]
+		public void Contains_NotContains()
+		{
+			// Arrange
+			var randList = new RandomList<int>() { 5 };
+
+			// Act
+			bool result = randList.Contains(100);
+
+			// Assert
+			Assert.False(result);
 		}
 	}
 }
