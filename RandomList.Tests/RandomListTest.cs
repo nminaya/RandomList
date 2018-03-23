822bb3555e1dd2490e3b23ec9390011fba439954
nminaya <nelsonminayaperez@gmail.com>
2018-03-23 19:09:05 -0400

    Adding IndexerT ests

--- RandomList.Tests/RandomListTest.cs
+++ RandomList.Tests/RandomListTest.cs
@@ -135,65 +135,6 @@ namespace RandomList.Tests
 			}
 		}
 
-		[Fact(DisplayName = nameof(Enumerator_GetEnumerator))]
-		public void Enumerator_GetEnumerator()
-		{
-			// Arrange
-			var randList = new RandomList<int>() { 1, 2, 3 };
-
-			// Act
-			var enumerator = randList.GetEnumerator();
-
-			// Assert
-			Assert.NotNull(enumerator);
-		}
-
-		[Fact(DisplayName = nameof(Enumerator_MoveNext_EmptyList))]
-		public void Enumerator_MoveNext_EmptyList()
-		{
-			// Arrange
-			var randList = new RandomList<int>();
-			var enumerator = randList.GetEnumerator();
-
-			// Act
-			bool result = enumerator.MoveNext();
-
-			// Assert
-			Assert.False(result);
-		}
-
-		[Fact(DisplayName = nameof(Enumerator_MoveNext_NotEmptyList))]
-		public void Enumerator_MoveNext_NotEmptyList()
-		{
-			// Arrange
-			var randList = new RandomList<int> { 1, 2, 3 };
-			var enumerator = randList.GetEnumerator();
-
-			// Act
-			bool result = enumerator.MoveNext();
-
-			// Assert
-			Assert.True(result);
-		}
-
-		[Fact(DisplayName = nameof(Enumerator_MoveNextThroughCollection))]
-		public void Enumerator_MoveNextThroughCollection()
-		{
-			// Arrange
-			var randList = new RandomList<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-			var enumerator = randList.GetEnumerator();
-			int count = 0;
-
-			// Act
-			while (enumerator.MoveNext())
-			{
-				// Assert
-				int current = enumerator.Current;
-				Assert.True(current == randList[count]);
-				count++;
-			}
-		}
-
 		[Fact(DisplayName = nameof(Randomizing))]
 		public void Randomizing()
 		{
@@ -213,70 +154,6 @@ namespace RandomList.Tests
 
 			// Ok if contains any false value
 			Assert.Contains(equalValues, v => !v);
-		}
-
-		[Fact(DisplayName = nameof(Indexs_GettingValues_OutOfRange_NegativeIndex))]
-		public void Indexs_GettingValues_OutOfRange_NegativeIndex()
-		{
-			// Arrange
-			var randList = new RandomList<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-
-			// Act
-			Action action = () =>
-			{
-				var value = randList[-5];
-			};
-
-			// Assert
-			Assert.Throws<IndexOutOfRangeException>(action);
-		}
-
-		[Fact(DisplayName = nameof(Indexs_GettingValues_OutOfRange_IndexBiggerThanLength))]
-		public void Indexs_GettingValues_OutOfRange_IndexBiggerThanLength()
-		{
-			// Arrange
-			var randList = new RandomList<int> { 1, 2 };
-
-			// Act
-			Action action = () =>
-			{
-				var value = randList[2];
-			};
-
-			// Assert
-			Assert.Throws<IndexOutOfRangeException>(action);
-		}
-
-		[Fact(DisplayName = nameof(Indexs_SettingValues_OutOfRange_NegativeIndex))]
-		public void Indexs_SettingValues_OutOfRange_NegativeIndex()
-		{
-			// Arrange
-			var randList = new RandomList<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-
-			// Act
-			Action action = () =>
-			{
-				randList[-5] = 10;
-			};
-
-			// Assert
-			Assert.Throws<IndexOutOfRangeException>(action);
-		}
-
-		[Fact(DisplayName = nameof(Indexs_SettingValues_OutOfRange_IndexBiggerThanLength))]
-		public void Indexs_SettingValues_OutOfRange_IndexBiggerThanLength()
-		{
-			// Arrange
-			var randList = new RandomList<int> { 1, 2 };
-
-			// Act
-			Action action = () =>
-			{
-				randList[2] = 100;
-			};
-
-			// Assert
-			Assert.Throws<IndexOutOfRangeException>(action);
 		}
 	}
 }
